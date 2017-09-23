4afa7b9f3364b7540161baeff82d79c78290a556
Matt Liberty <matt.liberty@jetperch.com>
2017-09-23 11:29:14 -0400

    Added block memory allocator.

---
+++ include/embc/memory/block.h
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2017 Jetperch LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ *
+ * @brief A block-based allocator.
+ */
+
+
+#ifndef EMBC_MEMORY_BLOCK_H_
+#define EMBC_MEMORY_BLOCK_H_
+
+/**
+ * @ingroup embc
+ * @defgroup embc_block Block-based allocator.
+ *
+ * @brief A block-based allocator.
+ *
+ * This block based allocator is a simple form of a heap.  It allocates
+ * the provided memory using fixed size blocks.  This allocator does suffer
+ * from fragmentation and variable (but bounded) allocation time.  This
+ * allocator is best used for objects with variable runtime size that are
+ * allocated and deallocated together.
+ *
+ * The allocation information (the struct embc_mblock_s) is stored
+ * separated from the managed memory which can be necessary in many cases.
+ * A typical use of this block based allocator is to manage the USB device
+ * endpoint memory buffers.
+ *
+ * This implementation is not thread safe.
+ *
+ * References include:
+ *
+ * @{
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "embc/cmacro_inc.h"
+
+EMBC_CPP_GUARD_START
+
+
+// Forward declaration for internal structure.
+struct embc_mblock_s;
+
+/**
+ * @brief Get the block instance size.
+ *
+ * @param mem_size The total size of the memory to manage.
+ * @param block_size The size of each block in bytes.
+ * @return The required size for embc_mblock_s in bytes.
+ */
+EMBC_API int32_t embc_mblock_instance_size(int32_t mem_size,
+                                           int32_t block_size);
+
+/**
+ * @brief Initialize a new block allocator.
+ *
+ * @param[out] self The memory block allocator to initialize which must be at
+ *      least embc_mblock_instance_size(mem_size, block_size) bytes.
+ * @param mem The memory region to manage.
+ * @param mem_size The total size of the memory to manage.
+ * @param block_size The size of each block in bytes.
+ * @return 0 or error code.
+ */
+EMBC_API int32_t embc_mblock_initialize(
+        struct embc_mblock_s * self,
+        void * mem,
+        int32_t mem_size,
+        int32_t block_size);
+
+/**
+ * @brief Finalize the memory block allocator instance.
+ *
+ * @param self The memory block allocator initialized by embc_mblock_initialize().
+ *
+ * This function does not free the instance memory as the allocated memory was
+ * provided to embc_mblock_initialize().
+ */
+EMBC_API void embc_mblock_finalize(struct embc_mblock_s * self);
+
+/**
+ * @brief Allocate memory.
+ *
+ * @param self The memory block allocator instance.
+ * @param size The required size from the pool
+ * @return The allocated memory.
+ *
+ * This function will ASSERT and not return on out of memory conditions.
+ */
+EMBC_API void * embc_mblock_alloc(struct embc_mblock_s * self, int32_t size);
+
+/**
+ * @brief Allocate memory.
+ *
+ * @param self The memory block allocator instance.
+ * @param size The required size from the pool
+ * @return The allocated memory or 0.  Application should use
+ *      embc_mblock_alloc() whenever out of memory is a fatal error.
+ */
+EMBC_API void * embc_mblock_alloc_unsafe(struct embc_mblock_s * self, int32_t size);
+
+/**
+ * @brief Free previously allocated memory.
+ *
+ * @param self The memory block allocator instance.
+ * @param buffer The previously allocated memory.
+ * @param size The size of buffer provided to the allocator
+ *
+ * This function must be called with the exact size provided to the allocator.
+ * Providing a different size may cause memory corruption.  The allocator
+ * is not capable of detecting duplicate frees.
+ */
+EMBC_API void embc_mblock_free(struct embc_mblock_s * self, void * buffer, int32_t size);
+
+
+EMBC_CPP_GUARD_END
+
+/** @} */
+
+#endif /* EMBC_MEMORY_POOL_H_ */
---
+++ src/memory/block.c
@@ -0,0 +1,101 @@
+
+#include "embc/memory/block.h"
+#include "embc/cdef.h"
+#include "embc/dbc.h"
+#include <string.h> // memset
+
+struct mblock_s {
+    uint8_t * mem;
+    int32_t mem_size;
+    int32_t block_size;
+    int32_t block_count;
+    uint8_t bitmap[4]; // dynamically allocated to be the right size!
+};
+
+int32_t embc_mblock_instance_size(int32_t mem_size, int32_t block_size) {
+    DBC_GT_ZERO(mem_size);
+    DBC_GT_ZERO(block_size);
+    int32_t blocks_count = mem_size / block_size;
+    int32_t bitmap_size = EMBC_ROUND_UP_TO_MULTIPLE(4, (blocks_count + 7) / 8);
+    if (bitmap_size >= 4) {
+        bitmap_size -= 4;
+    }
+    return (sizeof(struct mblock_s) + bitmap_size);
+}
+
+int32_t embc_mblock_initialize(
+        struct embc_mblock_s * self,
+        void * mem,
+        int32_t mem_size,
+        int32_t block_size) {
+    DBC_NOT_NULL(self);
+    DBC_NOT_NULL(mem);
+    DBC_GT_ZERO(mem_size);
+    DBC_GT_ZERO(block_size);
+    struct mblock_s * s = (struct mblock_s *) self;
+    int32_t sz = embc_mblock_instance_size(mem_size, block_size);
+    memset(s, 0, sz);
+    s->mem = (uint8_t *) mem;
+    s->mem_size = mem_size;
+    s->block_size = block_size;
+    s->block_count = mem_size / block_size;
+    return 0;
+}
+
+void embc_mblock_finalize(struct embc_mblock_s * self) {
+    (void) self;
+}
+
+static inline int32_t size_to_blocks(struct mblock_s * s, int32_t size) {
+    return (size + s->block_size - 1) / s->block_size;
+}
+
+void * embc_mblock_alloc_unsafe(struct embc_mblock_s * self, int32_t size) {
+    // greedy allocator: take first space large enough
+    struct mblock_s * s = (struct mblock_s *) self;
+    DBC_NOT_NULL(s);
+    DBC_GT_ZERO(size);
+    int32_t blocks = size_to_blocks(s, size);
+    int32_t idx_invalid = s->block_count + 1;
+    int32_t idx_start = idx_invalid;
+    int32_t free_count = 0;
+    for (int32_t idx_search = 0; idx_search < s->block_count; ++idx_search) {
+        int32_t bit = s->bitmap[idx_search / 8] >> (idx_search & 0x7);
+        if (bit) {
+            idx_start = idx_invalid;
+            free_count = 0;
+        } else if (idx_start == idx_invalid) {
+            idx_start = idx_search;
+            free_count = 1;
+        } else {
+            ++free_count;
+        }
+        if (free_count >= blocks) { // allocated, mark!
+            for (int idx_alloc = idx_start; idx_alloc <= idx_search; ++idx_alloc) {
+                s->bitmap[idx_alloc / 8] |= (uint8_t) (1 << (idx_alloc & 0x07));
+            }
+            return (s->mem + (idx_start * s->block_size));
+        }
+    }
+    return 0;
+}
+
+void * embc_mblock_alloc(struct embc_mblock_s * self, int32_t size) {
+    void * p = embc_mblock_alloc_unsafe(self, size);
+    EMBC_ASSERT_ALLOC(p);
+    return p;
+}
+
+void embc_mblock_free(struct embc_mblock_s * self, void * buffer, int32_t size) {
+    struct mblock_s * s = (struct mblock_s *) self;
+    uint8_t * b = (uint8_t *) buffer;
+    EMBC_ASSERT(b >= s->mem);
+    EMBC_ASSERT(b < (s->mem + s->mem_size));
+    int32_t blocks = size_to_blocks(s, size);
+    int32_t idx_start = (b - s->mem) / s->block_size;
+    for (int idx = idx_start; idx < idx_start + blocks; ++idx) {
+        int32_t bit = s->bitmap[idx / 8] >> (idx & 0x7);
+        EMBC_ASSERT(bit);  // ensure already allocated
+        s->bitmap[idx / 8] &= ~((uint8_t) (1 << (idx & 0x07)));
+    }
+}
